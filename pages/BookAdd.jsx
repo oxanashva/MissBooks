1e9bcc752d55aa2a68f7204dc8a7ecafb12fb6bf
oxanashva <oxanashav@gmail.com>
2025-09-07 18:07:27 +0300

    feat: Implement search with debounce, add clear search input button

--- pages/BookAdd.jsx
+++ pages/BookAdd.jsx
@@ -1,16 +1,25 @@
 import { bookService } from "../services/book.service.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
 import { googleBookService } from "../services/google-book.service.js"
+import { utilService } from "../services/util.service.js"
 
-const { useState } = React
+const { useState, useEffect, useRef } = React
 
 export function BookAdd() {
+    const [search, setSearch] = useState('')
     const [googleBooks, setGoogleBooks] = useState(null)
+
+    const debouncedSearch = useRef(utilService.debounce(onSearch)).current
+
+    useEffect(() => {
+        debouncedSearch(search)
+    }, [search])
 
     function onAddBook(bookToAdd) {
         bookService.addGoogleBook(bookToAdd)
             .then(savedBook => {
                 setGoogleBooks(prevGoogleBooks => prevGoogleBooks.filter(book => book.id !== bookToAdd.id))
+                setSearch('')
                 showSuccessMsg('Book added, book id:' + savedBook.id)
             })
             .catch(error => {
@@ -19,28 +28,55 @@ export function BookAdd() {
                 } else {
                     showErrorMsg('Cannot add book')
                 }
+                setSearch('')
                 console.error('Error:', error)
             })
     }
 
-    function onSearchSubmit(event) {
-        event.preventDefault()
-        googleBookService.query(event.target.search.value)
+    function handleInput(event) {
+        setSearch(event.target.value)
+    }
+
+    function clearInput() {
+        setSearch('')
+        setGoogleBooks(null)
+    }
+
+    function onSearch(searchTerm) {
+        if (!searchTerm) {
+            setGoogleBooks(null)
+            return
+        }
+        googleBookService.query(searchTerm)
             .then(setGoogleBooks)
     }
 
     return (
         <section className="book-add">
-            <h2>Book Add</h2>
-            <form onSubmit={onSearchSubmit}>
-                <input type="search" name="search" id="search" placeholder="Search Google Books" />
-            </form>
-            <ul>
-                {googleBooks && googleBooks.map(book => <li key={book.id}>
-                    <span>{book.volumeInfo.title}</span>
-                    <button onClick={() => onAddBook(book)}>+</button>
-                </li>)}
-            </ul>
+            <div className="container">
+                <h2 className="text-center">Book Add</h2>
+                <form>
+                    <input
+                        id="search"
+                        name="search"
+                        type="text"
+                        placeholder="Search Google Books"
+                        value={search}
+                        onChange={handleInput}
+                    />
+                    <button className="clear-btn" type="button" onClick={clearInput}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 6L6 18M6 6l12 12" /></svg>
+                    </button>
+                </form>
+                <ul>
+                    {googleBooks && googleBooks.map(book => <li key={book.id}>
+                        <span>{book.volumeInfo.title}</span>
+                        <button onClick={() => onAddBook(book)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill="currentColor" d="M12 5.25a.75.75 0 0 1 .75.75v5.25H18a.75.75 0 0 1 0 1.5h-5.25V18a.75.75 0 0 1-1.5 0v-5.25H6a.75.75 0 0 1 0-1.5h5.25V6a.75.75 0 0 1 .75-.75" /></svg>
+                        </button>
+                    </li>)}
+                </ul>
+            </div>
         </section>
     )
 }
